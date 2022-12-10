da479333a5084c1cc031df435881a7aa11b750ce
mijin0721 <kgmi2010@naver.com>
2022-12-11 04:20:15 +0900

    1.1.1

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ app.listen(8080, function() {
     console.log('listening on 8080')
 })
 
-app.get('/', function(req, res) { 
+app.get('/', function(req, res) { //메인 웹 페이지
     res.sendFile(__dirname +'/home.html')
+})
+
+app.get('/hospital', function(req, res) { // 병원확인
+    res.sendFile(__dirname +'/hospital.html')
+})
+
+app.get('/reservation', function(req, res) { //예약하기
+    res.sendFile(__dirname +'/reservation.html')
+})
+
+app.get('/res_confirm', function(req, res) { //예약확인
+    res.sendFile(__dirname +'/res_confirm.html')
 })
