529152eb8541f2bb281de7b96bf79e7b2deffd34
mijin0721 <kgmi2010@naver.com>
2022-12-14 02:15:26 +0900

    1.4.1

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true})) 
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true})) 
 
-app.listen(8080, function() {
-    console.log('listening on 8080')
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+MongoClient.connect("mongodb+srv://mijin:20207140@cluster0.wagohmp.mongodb.net/?retryWrites=true&w=majority", function(err, client){
+  if (err) return console.log(err)
+  db = client.db('hospital');
+
+  app.listen(8080, function() {
+    console.log('listening on 8080')
+  })
 })
+
+
 
 app.get('/', function(req, res) { //메인 웹 페이지
     res.sendFile(__dirname +'/home.html')
@@ -29,3 +42,15 @@ app.get('/signup', function(req, res) { //예약확인
     res.sendFile(__dirname +'/hos_signup.html')
 })
 
+app.get('/logout', function(req, res) { //예약확인
+    res.sendFile(__dirname +'/home_log.html')
+})
+
+app.post('/add', function(req, res){
+    db.collection('reservation').insertOne({name: req.body.name, phone: req.body.phone, hour: req.body.hour, 
+    minute: req.body.minute, hospital: req.body.hospital, symptom: req.body.symptom}, function(err, result){
+      if(err) return console.log("error");
+      console.log("save complete...");
+    })
+    res.sendFile(__dirname +'/home.html')
+  })
