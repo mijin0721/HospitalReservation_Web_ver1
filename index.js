f4d936f8c824ab1bb84a9f1b15c7666d20a91988
mijin0721 <kgmi2010@naver.com>
2022-12-14 16:07:48 +0900

    1.4.2

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const app = express();
 app.use(express.urlencoded({extended: true})) 
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true})) 
+var db;
 
 const MongoClient = require('mongodb').MongoClient
+app.set('view engine', 'ejs');
 
-var db;
+
 MongoClient.connect("mongodb+srv://mijin:20207140@cluster0.wagohmp.mongodb.net/?retryWrites=true&w=majority", function(err, client){
   if (err) return console.log(err)
   db = client.db('hospital');
-
+  console.log('DB connected')
   app.listen(8080, function() {
     console.log('listening on 8080')
   })
 })
-
-
 
 app.get('/', function(req, res) { //메인 웹 페이지
     res.sendFile(__dirname +'/html/home.html')
@@ -31,26 +31,44 @@ app.get('/reservation', function(req, res) { //예약하기
 })
 
 app.get('/res_confirm', function(req, res) { //예약확인
-    res.sendFile(__dirname +'/html/res_confirm.html')
+  db.collection('reservation').find().toArray(function(err, result){
+    console.log(result);
+    res.render('list.ejs', {loginfo : result})
+  })
 })
 
-app.get('/login', function(req, res) { //예약확인
+app.get('/login', function(req, res) { 
     res.sendFile(__dirname +'/html/hos_login.html')
 })
 
-app.get('/signup', function(req, res) { //예약확인
+app.get('/signup', function(req, res) { 
     res.sendFile(__dirname +'/html/hos_signup.html')
 })
 
-app.get('/logout', function(req, res) { //예약확인
+app.get('/logout', function(req, res) { 
     res.sendFile(__dirname +'/html/home_log.html')
 })
 
-app.post('/add', function(req, res){
+app.post('/add1', function(req, res){
     db.collection('reservation').insertOne({name: req.body.name, phone: req.body.phone, hour: req.body.hour, 
     minute: req.body.minute, hospital: req.body.hospital, symptom: req.body.symptom}, function(err, result){
       if(err) return console.log("error");
-      console.log("save complete...");
+      console.log("reservation save complete...");
     })
     res.sendFile(__dirname +'/html/home.html')
   })
+
+app.post('/add2', function(req, res){
+  db.collection('login').insertOne({email: req.body.email, password:req.body.password}, function(err, result){
+    if(err) return console.log("error");
+    console.log("login save complete...");
+  })
+  res.sendFile(__dirname +'/html/home.html')
+})
+
+//app.get('/list', function(req, res) {
+//  db.collection('reservation').find().toArray(function(err, result){
+//    console.log(result);
+//    res.render('list.ejs', {loginfo : result})
+//  })
+//})
